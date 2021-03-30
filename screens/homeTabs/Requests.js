37d7096436ebceed5f417fb948727a73e7adc395
aljt <alvinljt@gmail.com>
2021-03-30 16:23:56 +0800

    added functionality to retrieve a user's requests from the database

--- screens/homeTabs/Requests.js
+++ screens/homeTabs/Requests.js
@@ -1,10 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {
   View, TextInput, Text, Button,
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from '../../styles.js';
+import {
+  USER_AUTH, AppContext, retrieveUserRequests,
+} from '../../store.js';
 
 export default function RequestsScreen({ navigation }) {
+  const [isLoading, setIsLoading] = useState(true);
+  // retrieve the store variable and dispatch function from the App Context provider
+  const { store, dispatch } = useContext(AppContext);
+
+  // get the user's requests on the 1st time the component renders
+  useEffect(() => {
+    // try to get user authentication data from the phone's async storage
+    AsyncStorage.getItem(USER_AUTH).then((authData) => {
+      // parse the data. Null if no such item
+      const parsedAuthData = JSON.parse(authData);
+
+      // if there is a user authentication data set
+      if (parsedAuthData) {
+        // From database, retrieve exsisting requests made by the user
+        // and set it in the app provider's state
+        const retrieveUserRequestsResult = retrieveUserRequests(dispatch, parsedAuthData.userId);
+
+        retrieveUserRequestsResult.then((retrievedResult) => {
+          if (!(retrievedResult.error)) {
+            console.log(store.userRequests, 'store.userRequests');
+            // set loading to false if there is no error in getting the user's requests
+            setIsLoading(false);
+          } else {
+            // else theres an error, return to login screen
+            navigation.navigate('Login');
+          }
+        });
+      } else {
+        console.log('user is not authenticated');
+        // else theres no user authentication data, return to login screen
+        navigation.navigate('Login');
+      }
+    }).catch((error) => {
+      console.log(error);
+      // if theres an error, return to login screen
+      navigation.navigate('Login');
+    });
+  }, []);
+
+  // if app is still retrieving a user's requests
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Retrieving your requests...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Requests Screen</Text>
--- store.js
+++ store.js
@@ -1,8 +1,8 @@
 import React, { useReducer } from 'react';
 import axios from 'axios';
 
-// export const BACKEND_URL = 'http://localhost:3004';
-export const BACKEND_URL = 'https://freight-buddy-mobile-backend.herokuapp.com';
+export const BACKEND_URL = 'http://localhost:3004';
+// export const BACKEND_URL = 'https://freight-buddy-mobile-backend.herokuapp.com';
 // key for async storage to store a user's authentication details
 export const USER_AUTH = 'USER_AUTH';
 
@@ -10,11 +10,13 @@ export const USER_AUTH = 'USER_AUTH';
 export const initialState = {
   requests: [],
   selectedRequest: {},
+  userRequests: [],
 };
 
 // define each reducer action type we want to do on the data we defined above
 const RETRIEVE_REQUESTS = 'RETRIEVE_REQUESTS';
 const SET_REQUEST = 'SET_REQUEST';
+const RETRIEVE_USER_REQUESTS = 'RETRIEVE_USER_REQUESTS';
 
 // define the matching reducer function
 export function appReducer(state, action) {
@@ -23,6 +25,8 @@ export function appReducer(state, action) {
       return { ...state, requests: action.payload.requests };
     case SET_REQUEST:
       return { ...state, selectedRequest: action.payload.request };
+    case RETRIEVE_USER_REQUESTS:
+      return { ...state, userRequests: action.payload.requests };
     default:
       return state;
   }
@@ -43,6 +47,15 @@ export function setSelectedRequestAction(request) {
     type: SET_REQUEST,
     payload: {
       request,
+    },
+  };
+}
+
+export function retrieveUserRequestsAction(requests) {
+  return {
+    type: RETRIEVE_USER_REQUESTS,
+    payload: {
+      requests,
     },
   };
 }
@@ -83,11 +96,30 @@ export function AppProvider({ children }) {
  * ********************************
  * ********************************
  */
+// retrieve available requests to the user's country (i.e. requests with status='requested')
 export async function retrieveRequests(dispatch, userCountry) {
   return axios.get(`${BACKEND_URL}/requests`, { params: { country: userCountry } })
     .then((result) => {
       // update the state in AppProvider with the requests
       dispatch(retrieveRequestsAction(result.data.requestsList));
+      return { error: false };
+    })
+    .catch((error) => {
+      if (error.message === 'Request failed with status code 403') {
+        console.log('forbidden error');
+        return { error: true };
+      }
+      console.log(error);
+      return { error: true };
+    });
+}
+
+// retrieve a user's requests
+export async function retrieveUserRequests(dispatch, userId) {
+  return axios.get(`${BACKEND_URL}/users/${userId}/requests`)
+    .then((result) => {
+      // update the state in AppProvider with the user's requests
+      dispatch(retrieveUserRequestsAction(result.data.requestsList));
       return { error: false };
     })
     .catch((error) => {
